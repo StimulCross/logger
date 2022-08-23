520d07b9b8e198cbbc27e68b1f007f4d86d75dec
StimulCross <stimulcross@gmail.com>
2022-08-23 16:45:19 +0600

    chore(lint): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,10 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
+	extends: [
+		'@stimulcross/eslint-config-node',
+		'@stimulcross/eslint-config-typescript',
+		'@stimulcross/eslint-config-typescript/style'
+	],
 	parserOptions: {
-		project: 'tsconfig.json',
-		sourceType: 'module'
-	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
-	extends: ['plugin:@typescript-eslint/recommended'],
-	root: true,
-	env: {
-		node: true,
-		jest: true
-	},
-	ignorePatterns: ['.eslintrc.js'],
-	rules: {
-		'@typescript-eslint/no-empty-function': 'warn',
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'warn',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-inferrable-types': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/ban-types': 'off',
-		'@typescript-eslint/no-empty-interface': 'off'
+		project: './tsconfig.json'
 	}
 };
