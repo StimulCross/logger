23d80f3f3a5e4e972c02f93decd728c943588966
StimulCross <stimulcross@gmail.com>
2022-08-24 10:53:54 +0600

    chore(lint): update working path in prettier task in lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'*.{js,ts,css,json,md}': 'prettier --config ".prettierrc.js" --write ',
+	'src/*.{js,ts,css,json,md}': 'prettier --config ".prettierrc.js" --write ',
 	'src/*.{js,ts}': 'eslint'
 };
