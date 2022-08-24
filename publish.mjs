d302add5ea86ccd7b2002a948ce36d25fcdb6606
StimulCross <stimulcross@gmail.com>
2022-08-24 11:03:27 +0600

    chore: add publish script

---
+++ publish.mjs
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+// TAKEN FROM https://github.com/twurple/twurple/blob/main/publish.mjs
+
+import { exec as _exec, spawn } from 'child_process';
+import util from 'util';
+
+const exec = util.promisify(_exec);
+
+async function runYarn(args) {
+	const isWindows = /^win/.test(process.platform);
+	return runAndPassOutput(isWindows ? 'yarn.cmd' : 'yarn', args);
+}
+
+async function runAndPassOutput(cmd, args) {
+	return new Promise(resolve => {
+		const proc = spawn(cmd, args, {
+			stdio: 'inherit'
+		});
+		proc.on('exit', code => {
+			if (code) {
+				console.error(`subprocess exited with code ${code}; cmdline: ${[cmd, ...args].join(' ')}`);
+				process.exit(1);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+await runAndPassOutput('git', ['remote', 'update']);
+const localRev = (await exec('git rev-parse "@"')).stdout.trimEnd();
+const remoteRev = (await exec('git rev-parse "@{u}"')).stdout.trimEnd();
+const baseRev = (await exec('git merge-base "@" "@{u}"')).stdout.trimEnd();
+
+if (localRev !== remoteRev && remoteRev !== baseRev) {
+	console.log({ localRev, remoteRev, baseRev });
+	console.error('Your local repository is out of date; please pull');
+	process.exit(1);
+}
+
+await runYarn(['rebuild']);
+await runYarn(['lint']);
+await runYarn(['prettier:check']);
+
+const versionType = process.argv[2] ?? 'patch';
+
+await runYarn([
+	'npm',
+	'version',
+	'--commit-hooks',
+	'false',
+	'--preid',
+	'pre',
+	versionType,
+	'-m',
+	'build: release version %s'
+]);
+
+if (versionType.startsWith('pre')) {
+	await runYarn(['npm', 'publish', '--tag', 'next']);
+} else {
+	await runYarn(['npm', 'publish']);
+}
