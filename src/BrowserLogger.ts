970bebaf01e94649404e65c82c8a329805d683d7
Stimul Cross <stimulcross@gmail.com>
2024-09-18 14:44:54 +0500

    feat: upgrade browser logger

--- src/BrowserLogger.ts
+++ src/BrowserLogger.ts
@@ -1,24 +1,54 @@
-import type { LogLevel } from './LogLevel';
 import { BaseLogger } from './BaseLogger';
-import { LogLevelToConsoleFunction } from './utils/LogLevelMap';
+import type { LogLevel } from './LogLevel';
+import { LogLevelToColor, LogLevelToConsoleFunction, LogLevelToType, LogLevelToTypeColor } from './utils/LogLevelMap';
+import { createColorWrapper } from './utils/StylingFunction';
 
 /** @internal */
 export class BrowserLogger extends BaseLogger {
+	private readonly _accentColorWrapper = createColorWrapper('yellowBright');
+
 	log(level: LogLevel, ...args: unknown[]): void {
 		if (level > this._minLevel) {
 			return;
 		}
 
 		const logFn = LogLevelToConsoleFunction[level];
-		let builtMessage = `[${this._context}]`;
 
-		const message = args.map((arg: unknown) => String(arg)).join(' ');
-		builtMessage += message;
+		const useColors = this._colors;
+		const templateArgs: string[] = [];
+		const arrArgs: unknown[] = [];
 
-		if (this._timeDiff) {
-			builtMessage += BaseLogger._updateAndGetTimestampDiff();
+		if (this._applicationName) {
+			templateArgs.push(useColors ? LogLevelToColor[level]('%s') : '%s');
+			arrArgs.push(`[${this._applicationName}]`);
 		}
 
-		logFn(builtMessage);
+		if (this._timestamps) {
+			templateArgs.push('%s');
+			arrArgs.push(`${new Date().toLocaleString(undefined, this._dateTimeFormatOptions)}   `);
+		}
+
+		templateArgs.push(useColors ? LogLevelToTypeColor[level]('%s') : '%s');
+		arrArgs.push(LogLevelToType[level]);
+
+		templateArgs.push(useColors ? this._accentColorWrapper('%s') : '%s');
+		arrArgs.push(`[${this._context}]`);
+
+		args.forEach((arg: unknown) => {
+			if (typeof arg === 'object') {
+				templateArgs.push('%o');
+			} else {
+				templateArgs.push(useColors ? LogLevelToColor[level]('%s') : '%s');
+			}
+
+			arrArgs.push(arg);
+		});
+
+		if (this._timeDiff) {
+			templateArgs.push(useColors ? this._accentColorWrapper('%s') : '%s');
+			arrArgs.push(BaseLogger._updateAndGetTimestampDiff());
+		}
+
+		logFn(templateArgs.join(' '), ...arrArgs);
 	}
 }
