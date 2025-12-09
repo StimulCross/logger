c0fb88622543838bcc2332acc9e256ab2f3b5d09
Stimul Cross <stimulcross@gmail.com>
2025-12-10 00:08:59 +0500

    chore: update dependencies and configs

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	'src/*.{js,ts,css,json,md}': 'prettier --config ".prettierrc.js" --write ',
-	'src/*.{js,ts}': 'eslint'
-};
---
+++ eslint.config.mjs
@@ -0,0 +1,141 @@
+import node from '@stimulcross/eslint-config-node';
+import nodeStyle from '@stimulcross/eslint-config-node/style';
+import typescript from '@stimulcross/eslint-config-typescript';
+import typescriptStyle from '@stimulcross/eslint-config-typescript/style';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+const globs = {
+	js: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+	ts: ['**/*.ts', '**/*.cts', '**/*.mts'],
+	jsSpec: ['**/*.spec.js', '**/*.spec.cjs', '**/*.spec.mjs'],
+	tsSpec: ['**/*.spec.ts', '**/*.spec.cts', '**/*.spec.mts'],
+	lib: '**/lib',
+	nodeModules: '**/node_modules',
+	coverage: '**/coverage',
+	dts: '**/*.d.ts',
+};
+
+export default defineConfig(
+	globalIgnores([globs.lib, globs.nodeModules, globs.coverage, globs.dts]),
+	{
+		files: [...globs.js],
+		...node,
+	},
+	{
+		files: [...globs.js],
+		...nodeStyle,
+	},
+	{
+		files: [...globs.ts, ...globs.tsSpec],
+		...typescript,
+	},
+	{
+		files: [...globs.ts, ...globs.tsSpec],
+		...typescriptStyle,
+	},
+	{
+		files: [...globs.ts],
+		rules: {
+			'unicorn/prefer-native-coercion-functions': 'off',
+			'@typescript-eslint/explicit-member-accessibility': [
+				'error',
+				{
+					accessibility: 'explicit',
+					overrides: {
+						accessors: 'explicit',
+						constructors: 'no-public',
+						methods: 'explicit',
+						properties: 'off',
+						parameterProperties: 'explicit',
+					},
+				},
+			],
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			'@typescript-eslint/unified-signatures': 'off',
+		},
+	},
+	{
+		files: ['tests/**'],
+		rules: {
+			'id-length': 'off',
+			'no-console': 'off',
+			'max-classes-per-file': 'off',
+			'max-nested-callbacks': ['warn', { max: 10 }],
+			'unicorn/consistent-function-scoping': 'off',
+			'unicorn/no-useless-undefined': 'off',
+			'@typescript-eslint/class-literal-property-style': 'off',
+			'@typescript-eslint/explicit-member-accessibility': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/naming-convention': 'off',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/no-floating-promises': 'off',
+			'@typescript-eslint/no-loop-func': 'off',
+			'@typescript-eslint/no-extraneous-class': 'off',
+			'@typescript-eslint/no-unsafe-call': 'off',
+			'@typescript-eslint/no-unsafe-member-access': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/unbound-method': 'off',
+		},
+	},
+);
+
+// /** @type {import("eslint").Linter.Config[]} */
+// const config = [
+// 	node,
+// 	nodeStyle,
+// 	typescript,
+// 	typescriptStyle,
+// 	{
+// 		ignores: [globs.lib, globs.nodeModules, '**/*.d.ts'],
+// 	},
+// 	{
+// 		files: [...globs.js, ...globs.ts, ...globs.tsSpec, ...globs.jsSpec],
+// 		rules: {
+// 			'unicorn/no-await-expression-member': 'off',
+// 		},
+// 	},
+// 	{
+// 		files: [...globs.ts],
+// 		rules: {
+// 			'@typescript-eslint/explicit-member-accessibility': [
+// 				'error',
+// 				{
+// 					accessibility: 'explicit',
+// 					overrides: {
+// 						accessors: 'explicit',
+// 						constructors: 'no-public',
+// 						methods: 'explicit',
+// 						properties: 'off',
+// 						parameterProperties: 'explicit',
+// 					},
+// 				},
+// 			],
+// 			'@typescript-eslint/no-explicit-any': 'off',
+// 			'@typescript-eslint/no-non-null-assertion': 'off',
+// 			'@typescript-eslint/unified-signatures': 'off',
+// 		},
+// 	},
+// 	{
+// 		files: ['tests/**'],
+// 		rules: {
+// 			'id-length': 'off',
+// 			'no-console': 'off',
+// 			'max-classes-per-file': 'off',
+// 			'max-nested-callbacks': ['warn', { max: 10 }],
+// 			'unicorn/consistent-function-scoping': 'off',
+// 			'unicorn/no-useless-undefined': 'off',
+// 			'@typescript-eslint/class-literal-property-style': 'off',
+// 			'@typescript-eslint/naming-convention': 'off',
+// 			'@typescript-eslint/no-empty-function': 'off',
+// 			'@typescript-eslint/no-loop-func': 'off',
+// 			'@typescript-eslint/no-unused-vars': 'off',
+// 			'@typescript-eslint/explicit-member-accessibility': 'off',
+// 			'@typescript-eslint/explicit-function-return-type': 'off',
+// 		},
+// 	},
+// ];
+//
+// export  config;
