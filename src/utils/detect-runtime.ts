33fc73f2b0181d4657331294430290e13597deb3
Stimul Cross <stimulcross@gmail.com>
2025-12-13 10:03:26 +0500

    fix: improve runtime detection

--- src/utils/detect-runtime.ts
+++ src/utils/detect-runtime.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+
 /** @internal */
 export const enum Runtime {
 	Browser = 'browser',
@@ -9,23 +11,21 @@ export const enum Runtime {
 /** @internal */
 export function detectRuntime(): Runtime | null {
 	// @ts-ignore Browser detection
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	if (globalThis?.document !== undefined) {
 		return Runtime.Browser;
 	}
 
 	// @ts-ignore Deno detection
-	if (globalThis.Deno !== undefined) {
+	if (globalThis?.Deno !== undefined) {
 		return Runtime.Deno;
 	}
 
 	// @ts-ignore Bun detection
-	if (globalThis.Bun !== undefined) {
+	if (globalThis?.Bun !== undefined) {
 		return Runtime.Bun;
 	}
 
 	// Node.js detection
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	if (typeof process !== 'undefined' && process?.release?.name && process.release.name.toLowerCase() === 'node') {
 		return Runtime.Node;
 	}
