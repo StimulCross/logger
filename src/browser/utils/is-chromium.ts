1aa89f49441c3c10ac95a97d5786dab37c4396af
Stimul Cross <stimulcross@gmail.com>
2026-03-01 15:51:45 +0500

    fix: address global `navigator` reference in isChromium detection

--- src/browser/utils/is-chromium.ts
+++ src/browser/utils/is-chromium.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-const ua = navigator ? navigator.userAgent : undefined;
+const ua = globalThis.navigator ? globalThis.navigator.userAgent : undefined;
 
 /** @internal */
-export const isChromium = ua ? (ua.includes('Chrome') && !ua.includes('Firefox')) || ua.includes('Edg/') : true;
+export const isChromium = ua ? (ua.includes('Chrome') && !ua.includes('Firefox')) || ua.includes('Edg/') : false;
