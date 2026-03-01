aa7080ad47c4a6d308d13dd7b138bd3a1cd321f0
Stimul Cross <stimulcross@gmail.com>
2026-03-01 15:45:34 +0500

    test: add vitest config

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		environment: 'node',
+		include: ['tests/**/*.spec.ts'],
+		coverage: {
+			provider: 'v8',
+			reportsDirectory: './coverage',
+			include: ['src'],
+			exclude: ['src/**/index.ts', 'src/**/interfaces', 'src/**/types', 'src/**/enums'],
+		},
+	},
+});
