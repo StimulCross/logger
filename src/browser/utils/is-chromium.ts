b2d899040d92441f808af13145f4c31a6b91ca27
Stimul Cross <stimulcross@gmail.com>
2026-03-01 15:44:36 +0500

    fix: handle potential undefined navigator in isChromium detection

--- src/browser/utils/is-chromium.ts
+++ src/browser/utils/is-chromium.ts
@@ -1,4 +1,5 @@
-const ua = navigator.userAgent;
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+const ua = navigator ? navigator.userAgent : undefined;
 
 /** @internal */
-export const isChromium = (ua.includes('Chrome') && !ua.includes('Firefox')) || ua.includes('Edg/');
+export const isChromium = ua ? (ua.includes('Chrome') && !ua.includes('Firefox')) || ua.includes('Edg/') : true;
